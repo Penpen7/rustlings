42c62b5d8f051b55ba8cc78b93ef7aa28503063e
Penpen7 <Raosys@live.jp>
2022-12-11 20:13:26 +0900

    12/11 threads

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -3,8 +3,6 @@
 // Building on the last exercise, we want all of the threads to complete their work but this time
 // the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
 
-// I AM NOT DONE
-
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
@@ -16,10 +14,10 @@ struct JobStatus {
 fn main() {
     let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
-    for _ in 0..10 {
+    for i in 0..10 {
         let status_shared = status.clone();
         let handle = thread::spawn(move || {
-            thread::sleep(Duration::from_millis(250));
+            thread::sleep(Duration::from_millis(250 + 50 * i));
             let mut status_shared = status_shared.lock().unwrap();
             status_shared.jobs_completed += 1;
         });
